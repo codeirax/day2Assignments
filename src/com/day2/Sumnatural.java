af1b4ebfde65ddef2917ed07924dc4eea9b63ffe
codeirax <warmachinegaming4@gmail.com>
2022-05-17 22:53:05 +0530

    Method added NaturalSum

---
+++ src/com/day2/Sumnatural.java
@@ -0,0 +1,19 @@
+package com.day2;
+
+public class Sumnatural {
+
+	
+
+	public static void main(String[] args) {
+		int N = 10;
+		sumNatural(N);
+		
+	}
+	
+	public static void sumNatural(int N) {
+           N = 10;
+		
+		int result = N * (N+1)/2;
+		System.out.println(result);
+	}
+}
