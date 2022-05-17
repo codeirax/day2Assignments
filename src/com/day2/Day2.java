0786e080710e47feade807c8009d780bf0aa12a6
codeirax <warmachinegaming4@gmail.com>
2022-05-17 23:26:18 +0530

    methodAdded to city

--- src/com/day2/Day2.java
+++ src/com/day2/Day2.java
@@ -2,24 +2,32 @@ package com.day2;
 
 public class Day2 {
 	  public static void main(String[] args) {
-
-	   String city = "Kolkata";
-	    switch(city){
-
-	  case "Mumbai": System.out.println("Financial city");
-	    break;
-	  case "Kolkata": System.out.println("City of Joy");
-	    break;
-	  case "Delhi" : System.out.println("Capital of the country");
-	    break;
-	  case "Bangalore": System.out.println("Cyber City");
-	    break;
-
-	    default : System.out.println("May be Other Indian City");
-
-	  
-	}
+		  String city = "Bangalore";
+		  
+		  cityCall(city);
+	 
 	     
 
 	    }
+	  
+	  public static void cityCall(String city) {
+		  
+		
+		    switch(city){
+
+		  case "Mumbai": System.out.println("Financial city");
+		    break;
+		  case "Kolkata": System.out.println("City of Joy");
+		    break;
+		  case "Delhi" : System.out.println("Capital of the country");
+		    break;
+		  case "Bangalore": System.out.println("Cyber City");
+		    break;
+
+		    default : System.out.println("May be Other Indian City");
+
+		  
+		} 
+		  
+	  }
 	}
