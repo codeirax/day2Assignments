35114cb5cebb4b0dec4c3ae723983709b6ffe664
codeirax <warmachinegaming4@gmail.com>
2022-05-17 23:14:31 +0530

    Cricket Sum

---
+++ src/com/day2/Cricket.java
@@ -0,0 +1,22 @@
+package com.day2;
+
+public class Cricket {
+  public static void main(String[] args) {
+	  int one = 1;
+	  int two = 2;
+	  int three = 3;
+	  int four = 4;
+	  int five = 5;
+	  int six = 6;
+	  
+	  cricketSum(one,two ,three,four,five,six);
+}
+  
+  
+  public static void cricketSum( int one,int two,int three,int four,int five,int six){
+	  
+	  int result = one + two + three + four + five + six;
+	  
+	  System.out.println(result);
+  }
+}
